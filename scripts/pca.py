fix: move variance filter from preprocess to pca script
To avoid potential data leakage, variance filtering of genes has been moved after the data split in train and test data

scripts/pca.py
@@ -15,11 +15,19 @@ with open(snakemake.input.label_mapping) as f:
     label_mapping = json.load(f)
 inv_map = {int(k): v for k, v in label_mapping.items()}
 
+#filter for genes with high variance to reduce noise and dimensionality
+variance = X_train_scaled.var()
+high_var_genes = variance[variance > variance.median()].index
+X_train_scaled_filtered = X_train_scaled[high_var_genes]
+X_test_scaled_filtered = X_test_scaled[high_var_genes]
+with open(snakemake.log[0], 'w') as log:
+    log.write(f'Filtering leaves {len(X_train_scaled_filtered.columns)} genes\n')
+
 #fit PCA on training data only, save PCA and transform train and test data
 pca = PCA(n_components=n)
-X_train_pca = pca.fit_transform(X_train_scaled)
+X_train_pca = pca.fit_transform(X_train_scaled_filtered)
 joblib.dump(pca, snakemake.output.pca)
-X_test_pca = pca.transform(X_test_scaled)
+X_test_pca = pca.transform(X_test_scaled_filtered)
 
 #save compoments
 components_cols = [f'PC{i+1}' for i in range(n)]
@@ -28,12 +36,12 @@ pd.DataFrame(X_test_pca, columns=components_cols, index=X_test_scaled.index).to_
 
 #fit full PCA (all compoments) to determine variance explained
 pca_full = PCA()
-pca_full.fit(X_train_scaled)
+pca_full.fit(X_train_scaled_filtered)
 
 #get and log number of compoments to reach 80% variance
 cumsum = np.cumsum(pca_full.explained_variance_ratio_)
 n_components_80 = np.argmax(cumsum >= 0.8) + 1
-with open(snakemake.log[0], 'w') as log:
+with open(snakemake.log[0], 'a') as log:
     log.write(f'components for 80% variance: {n_components_80}\n')
     log.write(f'Variance explained by {n} components: {cumsum[n-1]:.3f}\n')
 

scripts/preprocess.py
@@ -29,13 +29,7 @@ with open(snakemake.output.label_mapping, 'w') as f:
 X = df.drop(columns=['Class'])
 X_log = np.log1p(X)
 
-#filter for genes with high variance to reduce noise and dimensionality
-variance = X_log.var()
-X_filtered = X_log[variance[variance > variance.median()].index]
-with open(snakemake.log[0], 'a') as log:
-    log.write(f'Filtering leaves {len(X_filtered.columns)} genes\n')
-
 #combine X and y for output and save to file
-X_filtered = pd.concat([X_filtered, df['Class']], axis=1) 
-X_filtered.to_csv(snakemake.output.data)
+X_log = pd.concat([X_log, df['Class']], axis=1) 
+X_log.to_csv(snakemake.output.data)
 